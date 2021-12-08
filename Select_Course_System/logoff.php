88372cead090bc4063dae41f600d44e9407f54b8
Malabis <75559487+Malabis@users.noreply.github.com>
2021-12-08 20:38:39 +0800

    Delete Select_Course_System directory

--- Select_Course_System/logoff.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once "mysql.php";
-
-setcookie('name','');
-echo "<script>location='login.html'</script>";
-
-$con->close();
-?>
--- Select_Course_System/mysql.php
+++
@@ -1,6 +0,0 @@
-<?php
-$servername = "";
-$username = "";
-$password = "";
-$dbname = "";
-$con = new mysqli($servername,$username,$password,$dbname);
